e16fe85412f1272930cd2bdd9ca88762259c5eca
sayan2110 <sayan.pal@wisdmlabs.com>
2025-08-31 16:24:14 +0530

    create category options

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 
 const addCategoryBtn = document.getElementById("exp-add-category");
+const addExpenseBtn = document.getElementById("exp-add-expense");
 
 addCategoryBtn.addEventListener("click", () => {
     const category = document.getElementById("exp-category") ;
@@ -12,15 +13,39 @@ addCategoryBtn.addEventListener("click", () => {
 
 
 addCategory = ( cat ) => {
+    const allCategory = fetchCategories();
+   
+    // console.log("allCategory ", allCategory);
+    const catOb = {
+        id: allCategory.length + 1,
+        category: cat
+    };
+
+    allCategory.push(catOb)
+    localStorage.setItem("categories", JSON.stringify(allCategory) );
+}
+
+fetchCategories = () => {
     let allCategory = JSON.parse( localStorage.getItem("categories") );
-    console.log("allCategory ", allCategory);
     
     if ( !allCategory )
         allCategory = [];
 
-    console.log("allCategory ", allCategory);
-
-
-    allCategory.push(cat)
-    localStorage.setItem("categories", JSON.stringify(allCategory) );
+    return allCategory;
 }
+
+createCategoryOptions = () => {
+    const allCategory = fetchCategories();
+    const selectEle = document.getElementById("exp-sel-category");
+
+    allCategory.forEach(item=> {
+        const optionEle = document.createElement("option");
+        optionEle.value = item.id;
+        optionEle.textContent = item.category
+        selectEle.appendChild(optionEle);
+    })
+
+}
+createCategoryOptions();
+
+
